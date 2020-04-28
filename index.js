1f26c73978cfa5e2dc8a8ebef5bfca4d4430181f
Liz P <parisielizabeth@gmail.com>
2020-04-28 23:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
 const names = ["Ada", "Brendan", "Ali"]
 const eventName = "birthday"
 
-function writeCards(names, eventName)
+function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]} for the wonderful ${eventName} gift!`);
+}
