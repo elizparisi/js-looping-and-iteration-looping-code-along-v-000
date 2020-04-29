769c7d1be76d9eaa37ab213baf7a1a4959fbeb30
Liz P <parisielizabeth@gmail.com>
2020-04-29 00:23:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solutions in this file
 
+
 const messages = [];
 
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-  
+
   return messages;
 }
-
